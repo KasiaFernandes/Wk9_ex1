3212b2d7ff544c6ee7281f0242134c3725ca6687
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-08-30 00:26:14 +0200

    Redundant spacing removed

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function getTriangleArea(a, h) {
 
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea (10, 15);
-var triangle2Area = getTriangleArea (7, 0);
-var triangle3Area = getTriangleArea (8, 8);
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 0);
+var triangle3Area = getTriangleArea(8, 8);
 
 console.log(triangle1Area, triangle2Area, triangle3Area);
